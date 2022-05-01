3d44885ae54317a3b8c4d650aa75bf267a620700
Scott <>
2022-04-30 21:11:43 -0600

    updated accounts controller

--- art_gallery/server/models/Account.js
+++ art_gallery/server/models/Account.js
@@ -6,8 +6,8 @@ export const AccountSchema = new Schema(
     subs: [{ type: String, unique: true }],
     email: { type: String, lowercase: true, unique: true },
     name: { type: String, required: true },
-    picture: { type: String }
-    // NOTE If you wish to add additional properties do so here
+    picture: { type: String },
+    userRole: { type: String, enum: ['Non-Admin', 'Admin'], required: true }
   },
   { timestamps: true, toJSON: { virtuals: true } }
 )
